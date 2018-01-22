ed18be79bc0b8f388f0a12ea4b91be093d7fe7e2
Anatolii Tupkalo <tigerforce@mail.ru>
2018-01-21 20:17:38 -0800

    weather-widget base has beeen built, all sections are finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,32 @@
 const slider = require('./moduls/slider.js');
 
-$(document).on('ready', function(){
-   slider(5);
-});
+window.onload = function(){
+    // var map;
+    // function initMap() {
+    //     map = new google.maps.Map(document.getElementById('map'), {
+    //         center: {lat: -34.397, lng: 150.644},
+    //         zoom: 8
+    //     });
+    // }
+
+    function initMap() {
+        var uluru = {lat: 39.7315155507, lng: -104.96951340};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 9,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    }
+
+    initMap();
+
+
+};
+
+
+
+
+
